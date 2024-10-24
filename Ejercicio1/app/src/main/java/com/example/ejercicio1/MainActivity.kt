e7b4389d8274486477375a01f7aa2883c3de7e43
Pablo Martínez Bienvenido <p.bienve01@gmail.com>
2024-10-24 18:57:14 +0200

    jc

--- Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
+++ Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
@@ -14,9 +12,7 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.ejercicio1.ui.components.screen
